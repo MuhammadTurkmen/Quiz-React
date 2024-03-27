1c1257b346c399d87e68dee698878f077b73833e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 08:09:36 +0430

    init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
+  const {waiting, loading, questions, index, correct} = useGlobalContext()
   return <h2>quiz starter</h2>
 }
 
