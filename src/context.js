1d11384a5233cb41d76340bdd386e2f6dbb9115c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 07:55:11 +0430

    init

--- src/context.js
+++ src/context.js
@@ -19,6 +19,7 @@ const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(false)
   const [questions, setQuestions] = useState([])
   const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
 
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
