2aa8c0fd8a226a546f6406b9b47f1cec913e6ad1
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 07:37:45 +0430

    init

--- src/context.js
+++ src/context.js
@@ -10,6 +10,7 @@ const table = {
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
+const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
 const AppContext = React.createContext()
 
