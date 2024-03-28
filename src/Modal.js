2b6be7b7dc1cc4e3ea6a0ebcca47057ec5269b70
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-28 11:24:24 +0430

    init

--- src/Modal.js
+++ src/Modal.js
@@ -4,7 +4,9 @@ import { useGlobalContext } from './context'
 const Modal = () => {
   const {isModaleOpen, closeModle, correct, questions} = useGlobalContext()
   return <div className={`${isModaleOpen ? 'modal-container isOpen': 'modal-container'}`}>
-    
+    <div className='modal-content' >
+      <h2>congrats</h2>
+    </div>
   </div>
 }
 
