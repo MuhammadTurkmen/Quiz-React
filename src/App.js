1696488360cda9ad9c8c972ae02950db9268f8b3
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-28 09:51:20 +0430

    init

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         </div>
 
       </article>
-      <button className='next-question'>next question</button>
+      <button className='next-question' onClick={nextQuestion}>next question</button>
     </section>
   </main>
 }
