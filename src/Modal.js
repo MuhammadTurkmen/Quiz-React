406d742363e374c198dafd505be7b01beeb4ec1e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-28 11:22:18 +0430

    init

--- src/Modal.js
+++ src/Modal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Modal = () => {
+  const {isModaleOpen, closeModle, correct, questions} = useGlobalContext()
   return <h2>modal component</h2>
 }
 
