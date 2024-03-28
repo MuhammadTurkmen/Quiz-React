130b3806f4ffc33291ab02084a963c9fc8fbc679
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-28 11:06:17 +0430

    init

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  const {waiting, loading, questions, index, correct, nextQuestion} = useGlobalContext()
+  const {
+    waiting, 
+    loading, 
+    questions, 
+    index, 
+    correct, 
+    nextQuestion,
+    checkAnswer
+  } = useGlobalContext()
   
   if(waiting) {
     return <SetupForm />  
