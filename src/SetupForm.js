993b15c6fcc9174910c5f9d4539963e85a5185e3
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-29 14:33:36 +0430

    init

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const SetupForm = () => {
+  const {quiz, handleChange, handleSubmit, error} = useGlobalContext()
   return <h2>setup form</h2>
 }
 
