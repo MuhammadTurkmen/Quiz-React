3dae12bab6b43cb49412b8fd544a9de460a11d95
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-29 22:14:05 +0430

    init

--- src/context.js
+++ src/context.js
@@ -89,6 +89,7 @@ const AppProvider = ({ children }) => {
   const handleChange = (e) => {
     const name = e.target.name
     const value = e.target.value
+    setQuiz({})
   }
 
   const handleSubmit = (e) => {
