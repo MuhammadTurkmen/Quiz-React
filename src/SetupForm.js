c3bc4d19374cb8db59ec153090b8c2061e6899a0
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-29 21:39:29 +0430

    init

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -10,7 +10,16 @@ const SetupForm = () => {
         {/* amount */}
         <div className='form-control'>
           <label htmlFor='amount'>number of questions</label>
-          <input type='number' />
+          <input 
+            type='number' 
+            name='amount' 
+            id='amount' 
+            value={quiz.amount} 
+            onChange={handleChange}
+            className='form-input'
+            min={1}
+            max={50}
+          />
         </div>
       </form>
     </section>
