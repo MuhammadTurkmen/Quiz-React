1cdc0dfe02ba4636d8489d85e7d9e440df89a77f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-29 14:03:07 +0430

    init

--- src/Modal.js
+++ src/Modal.js
@@ -6,7 +6,7 @@ const Modal = () => {
   return <div className={`${isModalOpen ? 'modal-container isOpen': 'modal-container'}`}>
     <div className='modal-content' >
       <h2>congrats!</h2>
-      <p>You answered {((correct/questions.length)*100).toFixed(0)} of questions correctly</p>
+      <p>You answered {((correct/questions.length)*100).toFixed(0)}% of questions correctly</p>
       <button className='close-btn' onClick={closeModle}>
         play again
       </button>
